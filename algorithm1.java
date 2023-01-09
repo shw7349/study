2644526cf8426268c4db47e0e9e94b3737efde52
HyunWoo <shw7349@naver.com>
2023-01-09 23:05:25 +0900

    20230109

--- algorithm1.java
+++
@@ -1,9 +0,0 @@
-//몫 구하기
-public class algorithm1 {
-    class Solution {
-        public int solution(int num1, int num2) {
-            int num3 = num1 / num2;
-            return num3;
-        }
-    }
-}
