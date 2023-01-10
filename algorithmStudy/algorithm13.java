b36608f1f2bb4271ea11453a8371161d75bfa6b1
HyunWoo <shw7349@naver.com>
2023-01-10 23:08:36 +0900

    20230110

---
+++ algorithmStudy/algorithm13.java
@@ -0,0 +1,12 @@
+package algorithmStudy;
+/* 종이 자르기/
+/*
+머쓱이는 큰 종이를 1 x 1 크기로 자르려고 합니다. 
+예를 들어 2 x 2 크기의 종이를 1 x 1 크기로 자르려면 최소 가위질 세 번이 필요합니다.
+ */
+class Solution {
+    public int[] solution(int num, int total) {
+        int[] answer = {};
+        return answer;
+    }
+}
